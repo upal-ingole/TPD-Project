986a715b281f5e1e39dcd7763afa30f547ebbe9e
upal ingole <upalingole22@gmail.com>
2024-11-12 23:54:20 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -50,3 +50,8 @@ app.use((err, req, res, next) => {
     message,
   });
 });
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
+});
