02de52210f9cd729b68a6f9affebe928fefd8baf
upal ingole <upalingole22@gmail.com>
2024-11-13 00:01:43 +0530

    updated

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,7 +1,7 @@
 import { Button } from 'flowbite-react'
 // import React from 'react'
 import { AiFillGoogleCircle } from 'react-icons/ai'
-import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { GoogleAuthProvider, getAuth, signInWithRedirect } from 'firebase/auth';
 import { app } from '../firebase';
 import { useDispatch } from 'react-redux';
 import { signInSuccess } from '../redux/user/userSlice';
@@ -15,7 +15,7 @@ export default function OAuth() {
         const provider = new GoogleAuthProvider();
         provider.setCustomParameters({ prompt: 'select_account' });
         try {
-            const resultsfromGoogle = await signInWithPopup(auth, provider);
+            const resultsfromGoogle = await signInWithRedirect(auth, provider);
             const res = await fetch('/api/auth/google', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
